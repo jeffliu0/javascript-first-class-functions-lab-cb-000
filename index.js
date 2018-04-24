81ff3ea9badda1f478b45fc8f6eb84d505c226e0
Jeffrey Liu <jeffreyliu0@gmail.com>
2018-04-24 04:40:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function createMultiplier(multiplierValue){
   return value=>{value*multiplierValue}
 }
 
-var doubler = createMultiplier(2)
+var doubler = n=> {n*createMultiplier(2)}
 var tripler = createMultiplier(3)
 
 function multiplier(value1, value2){
